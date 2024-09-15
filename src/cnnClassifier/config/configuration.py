998c02a4e386317153f7aebb8272c54f44437431
waelr1985 <waelr1985@gmail.com>
2024-09-16 01:17:55 +0300

    model trainer added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -5,6 +5,7 @@ from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 TrainingConfig,
                                                 EvaluationConfig)
 
+import os
 
 class ConfigurationManager:
     def __init__(
